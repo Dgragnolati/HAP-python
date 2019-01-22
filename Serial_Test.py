e6787743525ae42ad009ee70dedf0e61a165b8ad
David Gragnolati <David_Gragnolati@David-G-MBP-2016.local>
2019-01-21 20:23:33 -0800

    serial test script

---
+++ Serial_Test.py
@@ -0,0 +1,12 @@
+import serial
+
+port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3.0)
+
+rcv = port.read(10)
+print (rcv)
+
+while True:
+    port.write("start\n")
+    rcv = port.read(10)
+    print (rcv)
+    
