44685ac108640efa6574b8a438555dc5389e5e3f
David Gragnolati <David_Gragnolati@David-G-MBP-2016.local>
2019-01-21 20:46:21 -0800

    serial test script

--- Serial_Test.py
+++ Serial_Test.py
@@ -2,13 +2,15 @@ import serial
 
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3.0)
 
-rcv = port.read("17")
+rcv = port.read(17)
 print (rcv)
+print ("Requesting a Start")
+port.write(str.encode('start\n'))
+rcv = port.read(22)
 
 while True:
-    port.write(str.encode('start\n'))
-    rcv = port.read(10)
-    print (rcv)
     command=raw_input("Enter Command")
     command_newline=str(command)+'\n'
     port.write(str.encode('command_newline'))
+    rcv = port.read(1)
+    print (rcv)
