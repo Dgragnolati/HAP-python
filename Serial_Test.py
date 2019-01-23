fb1a3f2e07843147f3965409bf37f13b1607a26f
David Gragnolati <David_Gragnolati@David-G-MBP-2086.local>
2019-01-22 22:46:01 -0800

    serial test script

--- Serial_Test.py
+++ Serial_Test.py
@@ -5,15 +5,7 @@ port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3.0)
 
 
 
-port.write(str.encode('light_on\n'))
-rcv = port.readline()
-print (rcv)
-port.write(str.encode('pump_on\n'))
-rcv = port.readline()
-print (rcv)
-port.write(str.encode('light_off\n'))
-rcv = port.readline()
-print (rcv)
+
 port.write(str.encode('light\n'))
 rcv = port.readline()
 print (rcv)
