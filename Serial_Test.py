53ab10297350bfa95a0468f2fec457e075864bf7
David Gragnolati <David_Gragnolati@David-G-MBP-2086.local>
2019-02-02 16:40:18 -0800

    Added accessor PLant Love

--- Serial_Test.py
+++ Serial_Test.py
@@ -5,7 +5,15 @@ port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3.0)
 
 
 
-
+port.write(str.encode('light_on\n'))
+rcv = port.readline()
+print (rcv)
+port.write(str.encode('pump_on\n'))
+rcv = port.readline()
+print (rcv)
+port.write(str.encode('light_off\n'))
+rcv = port.readline()
+print (rcv)
 port.write(str.encode('light\n'))
 rcv = port.readline()
 print (rcv)
--- main_plant_love.py
+++ main_plant_love.py
@@ -14,7 +14,7 @@ from pyhap.accessory_driver import AccessoryDriver
 import pyhap.loader as loader
 from pyhap import camera
 from pyhap.const import CATEGORY_SENSOR
-from accessories.AM2302 import AM2302
+from accessories.PLantLove import PlantLoveAccessory
 
 logging.basicConfig(level=logging.DEBUG, format="[%(module)s] %(message)s")
 
