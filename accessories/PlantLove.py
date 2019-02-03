befd63aea74a40f950cd7304f0e2dca5e1da8ffd
David Gragnolati <David_Gragnolati@David-G-MBP-2086.local>
2019-02-03 11:15:29 -0800

    - Added more char, commented code + Udpated blocking function to live in a request to send info function

--- accessories/PlantLove.py
+++ accessories/PlantLove.py
@@ -27,17 +27,27 @@ class PlantLoveAccessory(Accessory):
 
         # Add the sprinkler
         serv_sprinkler = self.add_preload_service(
-            'Valve', chars=['Active','InUse','ValveType'])
+            'Valve', chars=['Active','InUse','ValveType','SetDuration',
+     'RemainingDuration',])
 
+    # Configure and set the valve type
         self.char_sprinkler_type = serv_sprinkler.configure_char(
             'ValveType')
         self.char_sprinkler_type.set_value(0)
+    #configure and set teh status/active (Inuse/Active)
         self.char_sprinkler_status = serv_sprinkler.configure_char(
             'InUse', setter_callback=self.set_sprinkler_status)
-        self.char_sprinkler_mode = serv_sprinkler.configure_char(
-            'Active', setter_callback=self.set_sprinkler_program)
+        self.char_sprinkler_active = serv_sprinkler.configure_char(
+            'Active', setter_callback=self.set_sprinkler_active)
+    # Confgue Duration and Remaining Duration
 
-        #Add grow lamp
+        self.char_sprinkler_duration = serv_sprinkler.configure_char(
+            'SetDuration', setter_callback=self.set_sprinkler_duration)
+
+        self.char_sprinkler_duration = serv_sprinkler.configure_char(
+            'RemainingDuration', setter_callback=self.get_sprinkler_duration)
+
+    #Add grow lamp light on there
         serv_growlamp = self.add_preload_service(
             'Lightbulb', chars=['On'])
         self.char_growlamp_status = serv_growlamp.configure_char(
@@ -45,38 +55,46 @@ class PlantLoveAccessory(Accessory):
 
         self.blocking=0
 
+    # Duration funcations
+    def get_sprinkler_duration(self,value):
+        logger.debug("Sprinkler duration has %s left", value)
+    def set_sprinkler_duration(self,value):
+        logger.debug("Sprinkler duration changed to %s", value)
+    # Sprinkler Status
     def set_sprinkler_status(self, value):
         logger.debug("Sprinkler status changed to %s", value)
 
-    def set_sprinkler_program(self, programmode):
-        logger.debug("Sprinkler duration set to %s", programmode)
+    def set_sprinkler_active(self, programmode):
+        logger.debug("Sprinkler activate status to %s", programmode)
 
     def set_growlamp_status(self, value):
-        logger.debug("Request to change state of growlamp")
+            if (value == 1):
+                #self.char_growlamp_status.set_value(self.send_command("light_on"))
+                value=self.request_to_send_command("light_on")
+                self.char_growlamp_status.set_value(value)
+            else if (value ==0):
+                #self.char_growlamp_status.set_value(self.send_command("light_off"))
+                value=self.request_to_send_command("light_off")
+                self.char_growlamp_status.set_value(value)
+            logger.debug("Grow lamp status changed %s", self.char_growlamp_status)
+
+
+    # checks to see if serial port is being blocked, if some other thread is using it wait to send command
+    def request_to_send_command(self,command):
         if (self.blocking ==0):
-            self.toggle_lamp(value)
+            logger.debug("serial not blocked")
+            return(self.send_command(command))
         else:
             while (self.blocking == 1):
                 logger.debug("Waiting to unblock %s", self.blocking)
                 sleep (2)
                 if (self.blocking ==0):
-                    self.toggle_lamp(value)
+                    return(self.send_command(command))
                     break
 
-
-    def toggle_lamp(self,value):
-            self.blocking=1
-            if (value == 1):
-                #self.char_growlamp_status.set_value(self.send_command("light_on"))
-                value=self.send_command("light_on")
-                self.char_growlamp_status.set_value(value)
-            if (value ==0):
-                #self.char_growlamp_status.set_value(self.send_command("light_off"))
-                value=self.send_command("light_off")
-                self.char_growlamp_status.set_value(value)
-            logger.debug("Grow lamp status changed %s", self.char_growlamp_status)
-
+    # Used to send commands to the serial port (Puts a blocking flag while in use)
     def send_command(self,command):
+        while:blcokign
         self.blocking=1
         port.flushInput()
         command_string=command+'\n'
@@ -96,29 +114,27 @@ class PlantLoveAccessory(Accessory):
     @Accessory.run_at_interval(500)
     def run(self):
         logger.debug("Starting Loop Function")
-        if self.blocking ==0:
-            logger.debug("serial not blocked")
+        # Request serial info from UART for moisture + light
+        current_moisture=self.request_to_send_command("moisture")
+        current_light=self.request_to_send_command("light")
+        # Log the moisture
+        logger.debug("Curret Moisture %s", current_moisture)
+        self.publish_to_log(MoistureLogPath,current_moisture)
+        # Log the light
+        logger.debug("Curret Light %s", current_light)
+        self.publish_to_log(LightLogPath,current_light)
 
-            current_moisture=self.send_command("moisture")
-            sleep(1)
-            current_light=self.send_command("light")
+        #if moisture is to low, go ahead and water the plants
 
-            # Log the moisture
-            logger.debug("Curret Moisture %s", current_moisture)
-            self.publish_to_log(MoistureLogPath,current_moisture)
-            # Log the light
-            logger.debug("Curret Light %s", current_light)
-            self.publish_to_log(LightLogPath,current_light)
+        if (current_moisture < 500):
 
-           #if moisture is to low, go ahead and water the plants
+            logger.debug("Turning Pump On")
+            pump_status=self.request_to_send_command("pump_on")
+            logger.debug("pump status %s", pump_status)
+            self.char_sprinkler_active.set_value(pump_status)
+            logger.debug("Pump Should Turn off in 2 Seconds")
+            sleep (2)
+            self.char_sprinkler_active.set_value(0)
 
-            if (current_moisture < 500):
-
-                logger.debug("Turning Pump On")
-                pump_status=self.send_command("pump_on")
-                logger.debug("pump status %s", pump_status)
-                self.char_sprinkler_status.set_value(pump_status)
-                logger.debug("Pump Should Turn off in 2 Seconds")
-
-            else:
-                logger.debug("water seems good")
+        else:
+            logger.debug("water seems good")
