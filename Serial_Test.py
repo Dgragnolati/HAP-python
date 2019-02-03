a3cc5ef639ae53b9bce0c02f58f9773d224ad60b
David Gragnolati <David_Gragnolati@David-G-MBP-2086.local>
2019-02-02 20:56:55 -0800

    Fire em all

--- Serial_Test.py
+++ Serial_Test.py
@@ -13,4 +13,9 @@ def send_command(command):
     #self.blocking=0
     return int(rcv.decode('utf-8'))
 
+
+print(send_command("light_on"))
+print(send_command("pump_on"))
+print(send_command("moisture"))
 print(send_command("light"))
+print(send_command("light_off"))
