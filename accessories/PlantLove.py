abc73cd759afec4bc2dfd4fb03816882bd81075a
David Gragnolati <David_Gragnolati@David-G-MBP-2086.local>
2019-02-02 18:51:20 -0800

    Comment out publish to log

--- accessories/PlantLove.py
+++ accessories/PlantLove.py
@@ -79,9 +79,9 @@ class PlantLoveAccessory(Accessory):
 
     @Accessory.run_at_interval(10)
     def run(self):
-
-        publish_to_log(self,LightLogPath,get_light_value())
-        publish_to_log(self,MoistureLogPath,get_light_value())
+        print "Starting Loop Function"
+        #publish_to_log(LightLogPath,get_light_value())
+        #publish_to_log(MoistureLogPath,get_light_value())
         print ("Curret Moisture %s", get_moisture_value())
         print ("Curret Light %s", get_light_value())
 
