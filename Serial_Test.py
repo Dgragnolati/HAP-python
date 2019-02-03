8016b1c83ea6eb5990c2a6a8c57319168c74be1e
David Gragnolati <David_Gragnolati@David-G-MBP-2086.local>
2019-02-02 20:54:07 -0800

    testing new serial function

--- Serial_Test.py
+++ Serial_Test.py
@@ -4,8 +4,13 @@ import sys
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3.0)
 
 
+def send_command(command):
+    #self.blocking=1
+    command_string=command+'\n'
+    port.write(str.encode(command_string))
+    rcv = port.readline()
+    port.flushInput()
+    #self.blocking=0
+    return int(rcv.decode('utf-8'))
 
-
-port.write(str.encode('light\n'))
-rcv = port.readline()
-print (str(rcv.decode('utf-8')))
+send_command("light")
